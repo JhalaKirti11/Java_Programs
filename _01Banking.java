b12a80aa27eb620fc133cef266bc9cb8046b88b0
JhalaKirti11 <kirtijhala1110@gmail.com>
2024-08-06 14:20:28 +0530

    Update _01Banking.java

--- _01Banking.java
+++ _01Banking.java
@@ -64,13 +64,11 @@ public class _01Banking{
         double withdraw = sc.nextDouble();
         SavingAccount sa = new SavingAccount(balance);
             sa.deposit(deposit);
-            sa.addInterest();           // working properly
+            sa.addInterest();
         CheckingAccount ca = new CheckingAccount(sa.balance);
             ca.withdraw(withdraw);
-           // sa.addInterest();           // not working properly
             double amount = ca.getBalance();
         System.out.println("Total balance is: "+amount);
         sc.close();
     }
-
 }
