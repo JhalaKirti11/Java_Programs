d4a2ef151076c3106fb367d3ad02c0cca4e9adeb
Kirti11 <kirtijhala1110@gmail.com>
2024-09-24 00:20:18 +0530

    Create MedianOfTwoArray.java

---
+++ MedianOfTwoArray.java
@@ -0,0 +1,49 @@
+
+
+public class MedianOfTwoArray {
+    public double median(int[] ar1, int[] ar2){
+        int size = ar1.length + ar2.length;
+            int[] ar3 = new int[size];
+
+// merge two arrays:
+            int k = 0;
+            for(int i = 0; i<size; i++){
+                if(i<ar1.length){
+                    ar3[i]= ar1[i]; 
+                }else{
+                    ar3[i] = ar2[k];
+                    k++;
+                }
+            }
+
+// sort the new array:
+         for(int i = 0; i<size; i++){
+            for(int j = 0; j<size-1; j++){
+                if(ar3[j]>ar3[j+1]){
+                    int temp= ar3[j];
+                    ar3[j] = ar3[j+1];
+                    ar3[j+1] = temp;
+                }
+            }
+         }
+    
+// find the median:
+
+    double sum = 0;
+    int a = size/2;
+    double median;
+        if(size%2==0){
+            for(int i = 0; i<size; i++){
+                System.out.print(ar3[i]+" ");
+                if(i==a||i==a-1){
+                    sum = sum+ar3[i];
+                }
+            }
+                median = sum/2;
+        }else{
+            median = ar3[a];
+        }
+
+        return median;
+}
+}
