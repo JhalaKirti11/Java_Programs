51a0e4a59184f6ee6e5236b3c97b701967041c61
Kirti11 <kirtijhala1110@gmail.com>
2024-10-01 01:39:37 +0530

    Create VehicleOverridingExample.java

---
+++ VehicleOverridingExample.java
@@ -0,0 +1,48 @@
+package Inheritance;
+
+class Vehicle{
+    String brand;
+    Vehicle(String brand){
+        this.brand = brand;
+    }
+
+    public void start(){
+        System.out.println("Vehicle started");
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+}
+
+class Car extends Vehicle{
+
+    public Car(String brand){
+        super(brand);
+    }
+    public void start(){
+        System.out.println("Car started");
+    }
+}
+
+class Bike extends Vehicle{
+
+    public Bike(String brand){
+        super(brand);
+    }
+    public void start(){
+        System.out.println("Bike started");
+    }
+}
+
+public class VehicleOverridingExample {
+    public static void main(String[] args) {
+        Vehicle c = new Car("Suzuki");
+        System.out.println("Brand of the car is "+c.getBrand());
+        c.start();
+
+        Vehicle b = new Bike("Hero");
+        System.out.println("Brand of the bike is "+b.getBrand());
+        b.start();
+    }
+}
