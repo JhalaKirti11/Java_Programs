148b8bdd5ba06a833c001f5b6cb0698cbaee6625
Kirti11 <kirtijhala1110@gmail.com>
2024-10-14 23:24:09 +0530

    Create PlindromeString.java

---
+++ PlindromeString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PlindromeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the string: ");
+        String str = sc.nextLine();
+        boolean k = true;
+        for(int i = 0; i< str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(str.length()-i - 1)){
+                k=false;
+                break;
+            }
+        }
+        if(k){
+            System.out.println(str+" is a palindrome.");
+        }else{
+            System.out.println(str+" is not a palindrome.");
+        }
+        sc.close();
+    }
+}
