ee0c0b2d8b3f556ddfba1a72914d6467382b944e
Kirti11 <kirtijhala1110@gmail.com>
2024-10-14 23:49:57 +0530

    Update and rename PlindromeString.java to PalindromeString.java

--- PalindromeString.java
+++ PalindromeString.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class PlindromeString {
+public class PalindromeString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter the string: ");
