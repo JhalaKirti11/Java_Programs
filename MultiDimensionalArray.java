10ab111757bffd2c5843bc9f23f7590e478fbb94
Kirti11 <kirtijhala1110@gmail.com>
2024-10-23 21:50:11 +0530

    Create MultiDimensionalArray.java

---
+++ MultiDimensionalArray.java
@@ -0,0 +1,18 @@
+public class MultiDimensionalArray {
+    public static void main(String[] args){
+    int[][] arr = new int[4][5];
+    int count = 1;
+        for(int i = 0; i<arr.length; i++){
+            for(int j = 0; j<arr[i].length;j++){
+                arr[i][j]= count;
+                count++;
+            }
+        }
+        for(int i = 0; i<arr.length; i++){
+            for(int j = 0; j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
