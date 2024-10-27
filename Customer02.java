3199fe09fc0f702a018e4c07ffef1755fb892481
Kirti11 <kirtijhala1110@gmail.com>
2024-10-27 16:36:23 +0530

    Create Customer02.java
    Customer with Serializable to read and write a file.

---
+++ Customer02.java
@@ -0,0 +1,39 @@
+import java.io.Serializable;
+
+class Customer02 implements Serializable{
+    private String name;
+    private int id;
+    private int phoneNo;
+    String address;
+
+    public Customer(String name, int id, int phoneNo, String address){
+        this.name = name;
+        this.id = id;
+        this.phoneNo = phoneNo;
+        this.address = address;
+    }
+        public void setName(String name){
+            this.name = name;
+        }
+        public String getName(){
+            return this.name;
+        }
+        public void setId(int id){
+            this.id = id;
+        }
+        public int getId(){
+            return this.id;
+        }
+        public void setPhoneNo(int phoneNo){
+            this.phoneNo = phoneNo;
+        }
+        public int getPhoneNo(){
+            return this.phoneNo;
+        }
+        public void setAddress(String address){
+            this.address = address;
+        }
+        public String getAddress(){
+            return this.address;
+        }
+    }
