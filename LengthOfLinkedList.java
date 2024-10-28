ea3359918759880f0b7564bc8e1d5051aa507a3e
Kirti11 <kirtijhala1110@gmail.com>
2024-10-29 00:29:45 +0530

    Create LengthOfLinkedList.java

---
+++ LengthOfLinkedList.java
@@ -0,0 +1,33 @@
+public class LengthOfLinkedList{
+    public static class Node {
+    int data;
+    Node next;
+    Node(int data){
+        this.data = data;
+    }
+}
+
+// length of the Nodes in LinkedList :-
+public static int linkedListLength(Node head){
+    int count = 0;
+    Node temp = head;
+    while (temp!= null){
+        count++;
+        temp = temp.next;
+    }
+    return count;
+}
+
+    public static void main(String[] args) {
+        Node n1 = new Node(5);
+        Node n2 = new Node(10);
+        Node n3 = new Node(8);
+        Node n4 = new Node(9);
+        Node n5 = new Node(3);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+      System.out.println("Length of the LinkedLust: "+linkedListLength(n1));
+    }
+}
