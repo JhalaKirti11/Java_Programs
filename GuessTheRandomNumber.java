c3cec155ee99f08175043f131036923727b2ed7d
Kirti11 <kirtijhala1110@gmail.com>
2024-11-03 20:58:20 +0530

    Create GuessTheRandomNumber.java

---
+++ GuessTheRandomNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class GuessTheRandomNumber {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Guess the number");
+
+        int numb = sc.nextInt();
+        int random = (int)Math.floor(Math.random() * 10) + 1;
+        while (numb != random) {
+            System.out.println("Wrong number guess again");
+            numb = sc.nextInt();
+
+            if (numb > random) {
+                System.out.println("The number is greater than the actual number!");
+            } else if (numb < random) {
+                System.out.println("The number is less than the actual number!");
+            } else {
+                System.out.println("Bingo! The corrent answer is " + random);
+            }
+        }
+        sc.close();
+    }
+}
