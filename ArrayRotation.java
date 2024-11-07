ca02ef726d4c7b848fc8802bd66cb62f4d6c7169
Kirti11 <kirtijhala1110@gmail.com>
2024-11-07 23:18:36 +0530

    Create ArrayRotation.java

---
+++ ArrayRotation.java
@@ -0,0 +1,40 @@
+// Rotate an array by a number of times given by user.
+
+import java.util.Scanner;
+public class ArrayRotation{
+    public void rotateArray(int[] num, int r){
+        int temp = 0;
+        for(int i =1; i <= r; i++){
+            for(int j = 0; j < num.length; j++){
+            if(j == 0){
+               temp = num[j];
+            }else if(j == num.length-1 && j != 0){
+                num[j-1] = num[num.length-1];
+                num[num.length-1] = temp;
+            }else{
+                num[j-1] = num[j];
+        }
+    }
+    }
+        for(int i = 0; i <  num.length; i++){
+            System.out.print(num[i]+" ");
+        }
+    }
+}
+public class _8RotateTheArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+        int[] arr = new int [size];
+        System.out.println("Enter the elements: ");
+        for(int i = 0; i < size; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter the no. to be rotate: ");
+        int d = sc.nextInt();
+        ArrayRotate ob = new ArrayRotate();
+        ob.rotateArray(arr,d);
+        sc.close();
+    }
+}
