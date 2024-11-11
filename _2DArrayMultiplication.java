c70a65421201c372759be333cc082456484140e3
Kirti11 <kirtijhala1110@gmail.com>
2024-11-11 23:37:52 +0530

    Create _2DArrayMultiplication.java

---
+++ _2DArrayMultiplication.java
@@ -0,0 +1,24 @@
+public class _2DArrayMultiplication {
+    public static void main(String[] args) {
+        int[][] arr1 = { { 1, 3 }, { 7, 4 } };
+        int[][] arr2 = { { 5, 9 }, { 2, 8 } };
+
+        int[][] result = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr2.length; k++) {
+                    result[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+// Print the resulting matrix
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
