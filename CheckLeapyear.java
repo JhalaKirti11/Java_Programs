838ea7efc5a96e2aae2cdba7b0edb3c2e6e9b9a2
Kirti11 <kirtijhala1110@gmail.com>
2024-12-01 23:52:00 +0530

    Create CheckLeapyear.java

---
+++ CheckLeapyear.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class _10LeapYear {
+    public static boolean leapYear(int y) {
+        if (y % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the Year: ");
+        int year = sc.nextInt();
+        boolean LP = leapYear(year);
+        if (LP) {
+            System.out.println(year + " is a Leap Year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+        sc.close();
+    }
+}
