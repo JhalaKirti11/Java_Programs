161942cc31fdc300d1d34293eea0e997b954a04f
Kirti11 <kirtijhala1110@gmail.com>
2024-12-14 00:00:11 +0530

    Create CreatedAClass.java

---
+++ accessModifiers/ModifiersFolder/CreatedAClass.java
@@ -0,0 +1,10 @@
+package modifiers;
+
+public class CreatedAClass {
+    public int a = 2;
+    public int b = 4;
+    protected void add(){
+        int addition = a+b;
+        System.out.println(addition);
+    } 
+}
