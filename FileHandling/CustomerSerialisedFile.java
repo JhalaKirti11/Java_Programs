b7148dfda16c555570a413f3a12162c6f6e657b2
Kirti11 <kirtijhala1110@gmail.com>
2024-12-26 16:34:28 +0530

    Create CustomerSerialisedFile.java

---
+++ FileHandling/CustomerSerialisedFile.java
@@ -0,0 +1,44 @@
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CustomerSerialisedFile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // to write the file
+        try {
+            FileOutputStream fout = new FileOutputStream("newfile.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fout);
+            System.out.println("Enter name, ID, phoneNo and address:");
+            String name = sc.nextLine();
+            int ID = sc.nextInt();
+            int phoneNo = sc.nextInt();
+            sc.nextLine();
+            String address = sc.nextLine();
+            Customer cob = new Customer(name, ID, phoneNo, address);
+            oos.writeObject(cob);
+            oos.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // to read the file:
+        try {
+            FileInputStream fin = new FileInputStream("newfile.txt");
+            ObjectInputStream ois = new ObjectInputStream(fin);
+            Customer cob = (Customer) ois.readObject();
+
+            System.out.println(cob.getName() + " " + cob.getId() + " " + cob.getPhoneNo() + " " + cob.getAddress());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            sc.close();
+        }
+    }
+}
