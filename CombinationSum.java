1c88f89172b81a53c7252a53d41bbb85e07980db
Kirti11 <kirtijhala1110@gmail.com>
2025-05-09 23:22:58 +0530

    Create CombinationSum.java

---
+++ CombinationSum.java
@@ -0,0 +1,18 @@
+/*  Given an array of distinct integers candidates and a target integer target,
+return a list of all unique combinations of candidates where the chosen numbers sum to target.
+You may return the combinations in any order. */
+
+import java.util.*;
+public class CombinationSum {
+    public static List<List<Integer>> combination(int[] arr, int target){
+        List<List<Integer>> arl = new ArrayList<>();
+
+        return arl;
+    }
+    
+    public static void main(String[] args) {
+        int[] arr ={1,2,3,7,4};
+        int target = 7;
+        combination(arr, target);
+    }
+}
